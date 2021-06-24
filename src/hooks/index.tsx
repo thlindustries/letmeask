83e786fe4127248d308c2f35556ff9437a707e29
root <thiago.kraetzer@brainweb.com.br>
2021-06-24 10:52:06 -0300

    Fix: removing ghosting comment

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,6 +1,5 @@
 import { AuthProvider } from './auth';
 
-// eslint-disable-next-line react/prop-types
 const AppProvider: React.FC = ({ children }) => (
   <AuthProvider>{children}</AuthProvider>
 );
